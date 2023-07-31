a4aea25e1bfe5b02e13e2388cbf39f641d2a51aa
Yixing Lin <184059914@qq.com>
2023-07-31 21:52:59 +0200

    leetcode: #15 3Sum solution

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # My LeetCode Exercises in Java
 
 ## Array
-- [#0001](https://youtu.be/KLlXCFG5TnA) TwoSums : [HashMap solution](src/main/java/org/example/array/TwoSums.java)
+- [#0001](https://youtu.be/KLlXCFG5TnA) TwoSum: [HashMap solution](src/main/java/org/example/array/TwoSums.java)
 - [#0121](https://youtu.be/1pkOgXD63yU) Best Time to Buy and Sell Stock: [Sliding Window](src/main/java/org/example/array/BestTimetoBuyandSellStock.java). Find
     local min and search for local max. 
 - [#0217](https://youtu.be/3OamzN90kPg) Contains Duplicate: [HashSet](src/main/java/org/example/array/ContainsDuplicate.java).
 - [#0053](https://youtu.be/5WZl3MMT0Eg) Maximum Subarray: [Previous subarray](src/main/java/org/example/array/MaximumSubarray.java) can't be negative. 
-
+- [#0015](https://youtu.be/jzZsG8n2R9A) 3Sum: [Sort input](src/main/java/org/example/array/ThreeSums.java), then simplify the problem to the 2Sum problem.
 ## Linked List
 
 ## String
--- src/main/java/org/example/array/ThreeSums.java
+++ src/main/java/org/example/array/ThreeSums.java
@@ -1,25 +1,36 @@
 package org.example.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSums {
 
     public static List solutions(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
-        for (int k=0; k < nums.length; k++) {
-            for (int j=k+1; j < nums.length; j++) {
-                for (int i=j+1; i < nums.length; i++) {
-                    if (nums[i]+nums[j]+nums[k] == 0) {
-                        List<Integer> subsolution = new ArrayList<>();
-                        subsolution.add(nums[i]);
-                        subsolution.add(nums[j]);
-                        subsolution.add(nums[k]);
-                        results.add(subsolution);
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1])      // skip duplicate solutions
+                continue;
+
+            // Two sums solution.
+            int l=i+1, r=nums.length-1;   // left-right pointers
+            while(l < r) {
+                int threeSums = nums[i] + nums[l] + nums[r];
+                if (threeSums > 0) {
+                    r -= 1;
+                } else if (threeSums < 0) {
+                    l += 1;
+                } else {
+                    List<Integer> curSolution = Arrays.asList(nums[i], nums[l], nums[r]);
+                    results.add(curSolution);
+                    l += 1;
+                    while(nums[l] == nums[l-1] && l < r) { // skip duplicate solutions
+                        l += 1;
                     }
                 }
             }
-
         }
         return results;
     }
@@ -27,6 +38,7 @@ public class ThreeSums {
 
     public static void main(String[] args) {
         int[] in =  {-1,0,1,2,-1,-4};
+        System.out.println(Arrays.toString(in));
         List<List<Integer>> out = ThreeSums.solutions(in);
         System.out.println(out);
     }
